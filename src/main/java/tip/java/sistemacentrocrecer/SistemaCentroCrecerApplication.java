9d5161bdccdd43b252508b130dc1fab1e2cc665f
Dekyim <adrv97@gmail.com>
2026-04-24 20:10:49 -0300

    Initial commit

---
+++ src/main/java/tip/java/sistemacentrocrecer/SistemaCentroCrecerApplication.java
@@ -0,0 +1,13 @@
+package tip.java.sistemacentrocrecer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCentroCrecerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCentroCrecerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tip/java/sistemacentrocrecer/SistemaCentroCrecerApplicationTests.java
@@ -0,0 +1,13 @@
+package tip.java.sistemacentrocrecer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCentroCrecerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
